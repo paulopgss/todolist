567ec58844bdcab5e0fd20011bdf78797d88a6f7
Paulo Sousa <paulopgss@gmail.com>
2024-09-17 20:09:01 -0300

    Add bcrypt password hashing for user creation

--- src/main/java/br/com/paulosousa/todolist/user/UserController.java
+++ src/main/java/br/com/paulosousa/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.paulosousa.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,10 @@ public class UserController {
         if (userAlreadyExist != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
