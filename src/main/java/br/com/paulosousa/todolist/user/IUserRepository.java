06bac03212c1e23d444d299b2c4e1b4830596d20
Paulo Sousa <paulopgss@gmail.com>
2024-09-17 20:01:10 -0300

    Update user repository and controller for user creation

--- src/main/java/br/com/paulosousa/todolist/user/IUserRepository.java
+++ src/main/java/br/com/paulosousa/todolist/user/IUserRepository.java
@@ -1,4 +1,9 @@
 package br.com.paulosousa.todolist.user;
 
-public interface IUserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
 }
